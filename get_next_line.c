3717eb86d7bb8f6792c401c47aae665c3108602a
Barbara Stalze <bastalze@c2r4p2.42vienna.com>
2025-11-26 14:17:11 +0100

    26. November

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,53 @@
 /*   By: bastalze <bastalze@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 12:43:51 by bastalze          #+#    #+#             */
-/*   Updated: 2025/11/21 18:33:59 by bastalze         ###   ########.fr       */
+/*   Updated: 2025/11/25 17:27:13 by bastalze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
-char	*finding_nl(fd)
+char	*make_remainder(static char *remainder, int fd)
 {
 	
+}
+
+char	*finding_nl(char *buffer, size_t i, int fd)
+{
+	char	*line;
+	size_t	multi;
+
+	multi = 2;
+	while (!(ft_str_i(buffer, '\n')))
+	{
+		i = 0;
+		i = read(fd, buffer, BUFFER_SIZE);
+		if (i == -1)
+			return (NULL);
+		buffer[i] = 0;
+		if (i > BUFFER_SIZE)
+			return (buffer);
+		line = malloc((BUFFER_SIZE * multi) + 1);
+		
+		multi++;
+	}
+}
+
+char	*first_buffer(int fd)
+{
+	char	buffer[BUFFER_SIZE + 1];
+	size_t	i;
+
+	i = 0;
+	i = read(fd, buffer, BUFFER_SIZE);
+	if (i == -1)
+		return (NULL);
+	buffer[i] = 0;
+	if (i > BUFFER_SIZE)
+		return (buffer);
+	if (!(ft_str_i(buffer, '\n')
+		finding_nl(buffer, i, fd);
+	if ((ft_str_i(buffer, '\n')))
+		make_remainder(ft_str_i(buffer, '\n'), fd);
 }
 
 char	*cc_remainder(static char *remainder, int fd)
@@ -21,7 +60,9 @@ char	*cc_remainder(static char *remainder, int fd)
 	char *line;
 	char *result;
 
-	line = finding_nl(fd);
+	line = first_buffer(fd);
+	if (line == NULL)
+		return (NULL);
 	result = ft_strjoin(remainder, line);
 	free(line);
 	return result;
@@ -29,7 +70,7 @@ char	*cc_remainder(static char *remainder, int fd)
 
 char	*get_next_line(int fd)
 {
-	static char	remainder[BUFFER_SIZE];
+	static char	remainder[BUFFER_SIZE + 1];
 
 	if (!remainder)
 		return (finding_nl(fd));
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,64 @@
 /*   By: bastalze <bastalze@student.42vienna.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 12:44:17 by bastalze          #+#    #+#             */
-/*   Updated: 2025/11/18 12:44:21 by bastalze         ###   ########.fr       */
+/*   Updated: 2025/11/25 12:57:57 by bastalze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "get_next_line.h"
 
+static int	ft_string(char *string, char const *s1, int s1len)
+{
+	int	i;
 
+	i = 0;
+	while (s1len > 0)
+	{
+		string[i] = s1[i];
+		i++;
+		s1len--;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		s1len;
+	int		s2len;
+	char	*string;
+	int		i;
+	int		j;
+
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	string = malloc(s1len + s2len + 1);
+	if (!string)
+		return (0);
+	i = ft_string(string, s1, s1len);
+	j = 0;
+	while (s2len > 0)
+	{
+		string[i] = s2[j];
+		i++;
+		j++;
+		s2len--;
+	}
+	string[i] = 0;
+	return (string);
+}
+
+int	ft_str_i(const char *s, int c)
+{
+	unsigned char	find;
+	size_t			strlen;
+
+	strlen = ft_strlen(s);
+	find = (unsigned char) c;
+	while (strlen + 1 > 0)
+	{
+		if (*s == find)
+			return (strlen);
+		s++;
+		strlen--;
+	}
+	return (0);
+}
