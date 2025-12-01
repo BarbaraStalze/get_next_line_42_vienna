3b6432c81a53a29c236e1a7b690c3412f9dde7ec
Barbara Stalze <barbara.stalze@protonmail.com>
2025-12-01 23:29:24 +0100

    Updated version of get_next_line.c at 1.12.2025

--- get_next_line.c
+++ get_next_line.c
@@ -11,33 +11,58 @@
 /* ************************************************************************** */
 #include "get_next_line.h"
 
-char	*make_remainder(static char *remainder, int fd)
+char	*make_remainder(static char *remainder, char *buffer, char *line)
 {
-	
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (buffer != '\n')
+	{
+		line [(strlen(line)) + i] = buffer[i];
+		i++;
+	}
+	line[(strlen(line)) + i] = '\n';
+	i++;
+	j = 0;
+	while (rest[i])
+	{
+		remainder[j] = buffer[i + j];
+		j++;
+	}
+	remainder[j] = 0;
+	return (line);
 }
 
-char	*finding_nl(char *buffer, size_t i, int fd)
+char	*finding_nl(static char * remainder, char *buffer, size_t i, int fd)
 {
 	char	*line;
 	size_t	multi;
 
-	multi = 2;
-	while (!(ft_str_i(buffer, '\n')))
+	multi = 1;
+	if (!(ft_str_i(buffer, '\n')))
 	{
-		i = 0;
 		i = read(fd, buffer, BUFFER_SIZE);
 		if (i == -1)
 			return (NULL);
 		buffer[i] = 0;
-		if (i > BUFFER_SIZE)
-			return (buffer);
 		line = malloc((BUFFER_SIZE * multi) + 1);
-		
+		// memcpy(line, buffer);
+		// line[(BUFFER_SIZE * multi) + 1] = 0;
+		if (i < BUFFER_SIZE)
+			return (line);
 		multi++;
+		finding_nl(buffer, i, fd);
+		
 	}
+	else if (ft_str_i(buffer, '\n'))
+	{
+		line = make_remainder(remainder, buffer, line);
+	}
+	return (line);
 }
 
-char	*first_buffer(int fd)
+char	*first_buffer(static char *remainder, int fd)
 {
 	char	buffer[BUFFER_SIZE + 1];
 	size_t	i;
@@ -49,23 +74,25 @@ char	*first_buffer(int fd)
 	buffer[i] = 0;
 	if (i > BUFFER_SIZE)
 		return (buffer);
-	if (!(ft_str_i(buffer, '\n')
-		finding_nl(buffer, i, fd);
-	if ((ft_str_i(buffer, '\n')))
-		make_remainder(ft_str_i(buffer, '\n'), fd);
+	return (finding_nl(remainder, buffer, i, fd));
 }
 
 char	*cc_remainder(static char *remainder, int fd)
 {
 	char *line;
 	char *result;
+	char line[BUFFER_SIZE + 1];
 
-	line = first_buffer(fd);
+	if (ft_str_i(remainder, '\n'))
+		//bzero(line);
+		return (make_remainder(remainder, remainder, line);
+	line = first_buffer(remainder, fd);
 	if (line == NULL)
 		return (NULL);
 	result = ft_strjoin(remainder, line);
+	//remainder = 0 //bzero(remainder);
 	free(line);
-	return result;
+	return (result);
 }
 
 char	*get_next_line(int fd)
